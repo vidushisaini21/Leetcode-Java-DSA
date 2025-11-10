9eabf2db738124f514ed14e0ca49d677c65af684
vidushisaini21 <vidushis2003@gamil.com>
2025-11-10 16:06:18 +0530

    new

---
+++ Searching & Sorting/Sol_69.java
@@ -0,0 +1,16 @@
+// Leetcode 69: sqrt(x)
+// problem link : https://leetcode.com/problems/sqrtx/description/
+
+
+class Solution {
+    public int mySqrt(int x) {
+        if (x == 0) return 0;
+
+        long r = x;
+        while (r * r > x) {
+            r = (r + x / r) / 2;
+        }
+        return (int) r;
+    }
+}
+
